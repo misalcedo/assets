abc02b83264946c3d28838e4a9a32784e4c3f1f5
Miguel D. Salcedo <miguel@salcedo.cc>
2025-08-15 16:39:22 -0400

    Fix column name.

--- src/db/sql/balances.sql
+++ src/db/sql/balances.sql
@@ -5,6 +5,6 @@ WINDOW
     my_window AS (PARTITION BY nickname ORDER BY balance_as_of DESC)
 QUALIFY
     row_number() OVER my_window == 1
-ORDER BY account ASC
+ORDER BY nickname ASC
 LIMIT ?
 OFFSET ?;
